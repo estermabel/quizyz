84532459616c0faa77b45a9885699859be94b8c2
estermabel <estermabel2@gmail.com>
2021-05-11 20:18:52 -0300

    splash page ready to go

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:quizyz/pages/controller_page.dart';
+import 'package:quizyz/utils/style/animations/fade_animation.dart';
+import 'package:quizyz/utils/style/colors.dart';
 
 class SplashPage extends StatefulWidget {
   @override
@@ -11,7 +13,7 @@ class _SplashPageState extends State<SplashPage> {
   @override
   void initState() {
     super.initState();
-    _loadData();
+    //_loadData();
   }
 
   @override
@@ -32,13 +34,51 @@ class _SplashPageState extends State<SplashPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).backgroundColor,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text("teste")
-          // FadeAnimation(0.7, _icon()),
-          // FadeAnimation(1.4, _textTitle()),
-        ],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 128, bottom: 64),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Column(
+                children: [
+                  FadeAnimation(0.4, _image()),
+                  FadeAnimation(1.4, _textTitle()),
+                ],
+              ),
+              FadeAnimation(2.1, _textSubTitle()),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _image() {
+    return Image.asset("images/splash_image.png");
+  }
+
+  Widget _textTitle() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 64),
+      child: Text(
+        "Quizyz",
+        style: Theme.of(context).textTheme.headline2.copyWith(
+              color: accentColor,
+              fontWeight: FontWeight.bold,
+            ),
+      ),
+    );
+  }
+
+  Widget _textSubTitle() {
+    return Padding(
+      padding: const EdgeInsets.only(left: 64, right: 64),
+      child: Text(
+        "Ester Mabel, Alécio Barreto, Ary Sault, Yllo Luís, Michel de Jesus",
+        style: Theme.of(context).textTheme.subtitle1,
+        textAlign: TextAlign.center,
       ),
     );
   }
