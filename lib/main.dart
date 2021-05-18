ab171300049dd7d7d49a579f71d6f97fcad1a278
Ary Sayd Sault <sault.sayd.ary@gmail.com>
2021-05-17 23:25:27 -0300

    RankingPage e RankingBloc criados

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quizyz/db/data_base_helper.dart';
+import 'package:quizyz/pages/home/game/ranking_page.dart';
 import 'package:quizyz/pages/splash_page.dart';
 import 'package:quizyz/utils/style/themes/base_theme.dart';
 
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       title: 'Quizyz',
       debugShowCheckedModeBanner: false,
       theme: baseTheme,
-      home: SplashPage(),
+      home: RankingPage(),
     );
   }
 }
