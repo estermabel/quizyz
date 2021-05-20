1759faa1294d2e7da618861acaa633a6461a7508
estermabel <estermabel2@gmail.com>
2021-05-19 22:20:38 -0300

    exibir jogadores no score pronto, ajuste no leading da play_page

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:quizyz/db/config/data_base_helper.dart';
-import 'package:quizyz/pages/home/game/ranking_page.dart';
 import 'package:quizyz/pages/splash_page.dart';
 import 'package:quizyz/utils/style/themes/base_theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await DatabaseHelper.instance.database;
-  runApp(MyApp());
+  await DatabaseHelper.instance.database.then(
+    (value) => runApp(
+      MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
